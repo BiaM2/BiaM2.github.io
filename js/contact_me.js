119e2445818aa780c8ae2992ca8002f3b304901e
Beatriz Mamani <37555831+BiaM2@users.noreply.github.com>
2018-03-21 19:07:23 -0400

    par

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "mail/contact_me.php",
+				//url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -33,7 +34,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Tu mensaje ha sido enviado </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -45,7 +46,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Lo sentimos " + firstName + ", parece que nuestro servidor de correo no responde. ¡Por favor, inténtelo de nuevo más tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");

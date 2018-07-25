e9a50b5edeeffceceab78b566ce2e2c695793840
Beatriz Mamani <37555831+BiaM2@users.noreply.github.com>
2018-07-25 15:58:54 -0400

    cambio

--- config/app.example.js
+++
@@ -1,7 +0,0 @@
-module.exports  = {
-    encryption_key      : '{{APP_ENCRYPTION_KEY}}',
-    page_size           : '{{APP_PAGE_SIZE}}',
-    uploads_path        : '{{APP_UPLOADS_PATH}}',
-    uploads_tmp_path    : '{{APP_TMP_PATH}}',
-    host_name           : '{{APP_HOST_NAME}}'
-};
--- public/js/apis/ApisService.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-define( function () {
-    return function ( BaseService ) {
-        var ApisService = new BaseService( 'apis' );
-
-        return ApisService;
-    };
-});

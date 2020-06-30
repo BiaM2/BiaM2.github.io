d5587bda7be88f8735cab1ce9e5433ec402b00c9
Amiga-tierra <amiga2030tierra@gmail.com>
2020-06-30 17:03:18 -0400

    islp

--- datum/R/ietadata.R
+++
@@ -1,9 +0,0 @@
-##############
-#ietadata
-#autor: Alvaro Chirino
-##############
-ietadata<-function(key="eh17v"){
-  url<-paste0("https://ieta-umsa.github.io/datum/data/",key,".csv")
-  data<-read.csv2(url,stringsAsFactors=F)
-  return(data)
-}
